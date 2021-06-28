c407fc4057336f1c444c8f079281b5186a02ee9d
qaman88 <qaman88@hotmail.com>
2021-06-28 04:59:08 +0200

    Visual Stuudio Setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,68 +2,60 @@ using System;
 
 namespace ExpertWaves
 {
-    public class GameManager
-    {
-        private double score;
+	public class GameManager
+	{
+		private double score;
 
-        private float angle;
+		private float angle;
 
-        private DIRECTION direction;
+		private DIRECTION direction
+		{
+			get; set;
+		}
 
-        private Random randomGenerator;
+		private Random randomGenerator;
 
-        public GameManager()
-        {
-            this.randomGenerator = new Random();
-            this.Reset();
-        }
+		public GameManager()
+		{
+			this.randomGenerator = new Random();
+			this.Reset();
+		}
 
-        public void Reset()
-        {
-            this.score = 0;
-            this.angle = 0;
-            this.direction = DIRECTION.RIGHT;
-        }
+		public void Reset()
+		{
+			this.score = 0;
+			this.angle = 0;
+			this.direction = DIRECTION.RIGHT;
+		}
 
-        public float getRandomAngle()
-        {
-            double rightAngle = 90;
-            int scaler = randomGenerator.Next(0, 4);
-            this.angle = (float)(scaler * rightAngle);
-            switch (scaler)
-            {
-                case 0: //  0
-                    this.direction = DIRECTION.RIGHT;
-                    break;
-                case 1: // 90
-                    this.direction = DIRECTION.UP;
-                    break;
-                case 2: // 180
-                    this.direction = DIRECTION.LEFT;
-                    break;
-                case 3: // 270
-                    this.direction = DIRECTION.DOWN;
-                    break;
-                default: // 360
-                    break;
-            }
+		public float getRandomAngle()
+		{
+			double rightAngle = 90;
+			int scaler = randomGenerator.Next(0, 4);
+			this.angle = (float) ( scaler * rightAngle );
+			switch (scaler) {
+				case 0: //  0
+					this.direction = DIRECTION.RIGHT;
+					break;
+				case 1: // 90
+					this.direction = DIRECTION.UP;
+					break;
+				case 2: // 180
+					this.direction = DIRECTION.LEFT;
+					break;
+				case 3: // 270
+					this.direction = DIRECTION.DOWN;
+					break;
+				default: // 360
+					break;
+			}
 
-            return this.angle;
-        }
+			return this.angle;
+		}
 
-        public DIRECTION getDirection()
-        {
-            return this.direction;
-        }
-
-        public float getAngle()
-        {
-            return this.angle;
-        }
-
-        public double getScore()
-        {
-            return this.score;
-        }
-    }
+		internal DIRECTION getDirection()
+		{
+			return this.direction;
+		}
+	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,126 +1,109 @@
-using System.Collections;
-using System.Collections.Generic;
 using ExpertWaves;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class Player :MonoBehaviour
 {
-    // touch control
-    public double swipeRange = 50;
+	// touch control
+	public double swipeRange = 50;
 
-    public double tapRange = 10;
+	public double tapRange = 10;
 
-    private Vector2 startPosition;
+	private Vector2 startPosition;
 
-    private Vector2 currentPosition;
+	private Vector2 currentPosition;
 
-    private Vector2 endPosition;
+	private Vector2 endPosition;
 
-    private bool stopTouch = false;
+	private bool stopTouch = false;
 
-    // Optotype
-    public Image imageOptotype;
+	// Optotype
+	public Image imageOptotype;
 
-    public Text status;
+	public Text status;
 
-    // game manager
-    private GameManager gameManager;
+	// game manager
+	private GameManager gameManager;
 
-    private Vector3 scaling = new Vector3(0.01f, 0.01f, 1);
+	private Vector3 scaling = new Vector3(0.01f, 0.01f, 1);
 
-    private Vector3 scalingFactor = new Vector3(0.01f, 0.01f, 1);
+	private Vector3 scalingFactor = new Vector3(0.01f, 0.01f, 1);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameManager = new GameManager();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		gameManager = new GameManager();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Swipe();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		Swipe();
+	}
 
-    public void Swipe()
-    {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            startPosition = Input.GetTouch(0).position;
-        }
-        else if (
-            Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved
-        )
-        {
-            currentPosition = Input.GetTouch(0).position;
-            Vector2 distance = currentPosition - startPosition;
+	public void Swipe()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+			startPosition = Input.GetTouch(0).position;
+		}
+		else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			currentPosition = Input.GetTouch(0).position;
+			Vector2 distance = currentPosition - startPosition;
 
-            if (!stopTouch)
-            {
-                if (distance.x < -swipeRange)
-                {
-                    Debug.Log("Swipe left");
-                    stopTouch = true;
-                    this.NextLevel(DIRECTION.LEFT);
-                }
-                else if (distance.x > swipeRange)
-                {
-                    Debug.Log("Swipe right");
-                    stopTouch = true;
-                    this.NextLevel(DIRECTION.RIGHT);
-                }
-                else if (distance.y < -swipeRange)
-                {
-                    Debug.Log("Swipe down");
-                    stopTouch = true;
-                    this.NextLevel(DIRECTION.DOWN);
-                }
-                else if (distance.y > swipeRange)
-                {
-                    Debug.Log("Swipe up");
-                    stopTouch = true;
-                    this.NextLevel(DIRECTION.UP);
-                }
-            }
-        }
-        else if (
-            Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended
-        )
-        {
-            endPosition = Input.GetTouch(0).position;
-            Vector2 distance = endPosition - startPosition;
-            stopTouch = false;
+			if (!stopTouch) {
+				if (distance.x < -swipeRange) {
+					Debug.Log("Swipe left");
+					stopTouch = true;
+					this.NextLevel(DIRECTION.LEFT);
+				}
+				else if (distance.x > swipeRange) {
+					Debug.Log("Swipe right");
+					stopTouch = true;
+					this.NextLevel(DIRECTION.RIGHT);
+				}
+				else if (distance.y < -swipeRange) {
+					Debug.Log("Swipe down");
+					stopTouch = true;
+					this.NextLevel(DIRECTION.DOWN);
+				}
+				else if (distance.y > swipeRange) {
+					Debug.Log("Swipe up");
+					stopTouch = true;
+					this.NextLevel(DIRECTION.UP);
+				}
+			}
+		}
+		else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			endPosition = Input.GetTouch(0).position;
+			Vector2 distance = endPosition - startPosition;
+			stopTouch = false;
 
-            if (
-                Mathf.Abs(distance.x) < tapRange &&
-                Mathf.Abs(distance.y) < tapRange
-            )
-            {
-            }
-        }
-    }
+			if (Mathf.Abs(distance.x) < tapRange && Mathf.Abs(distance.y) < tapRange) {
+			}
+		}
+	}
 
-    public void NextLevel(DIRECTION direction)
-    {
-        if (direction == gameManager.getDirection())
-        {
-            // status text
-            this.status.text = "Play";
+	public void NextLevel(DIRECTION direction)
+	{
+		if (direction == gameManager.getDirection()) {
+			// status text
+			this.status.text = "Play";
 
-            // change angle
-            float angle = gameManager.getRandomAngle();
-            Debug.Log("### Angle " + angle);
-            this.imageOptotype.transform.localEulerAngles =
-                new Vector3(0f, 0f, angle);
+			// change angle
+			float angle = gameManager.getRandomAngle();
+			Debug.Log("### Angle " + angle);
+			this.imageOptotype.transform.localEulerAngles =
+					new Vector3(0f, 0f, angle);
 
-            // change size
-            imageOptotype.transform.localScale -= scalingFactor;
-        }
-        else if (direction != gameManager.getDirection())
-        {
-            // status text
-            this.status.text = "Try again";
-        }
-    }
+			// change size
+			this.imageOptotype.transform.localScale -= scalingFactor;
+			//Vector3 scale = this.imageOptotype.transform.localScale;
+			//Debug.Log(message: $"{scale.x},{scale.y},{scale.z}");
+		}
+		else if (direction != gameManager.getDirection()) {
+			// status text
+			this.status.text = "Try again";
+		}
+	}
 }
