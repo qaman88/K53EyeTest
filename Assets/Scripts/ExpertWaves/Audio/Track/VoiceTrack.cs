c591c9f8f64952aaa1817502f28a0e8859f9a48f
qaman88 <qaman88@hotmail.com>
2021-07-08 22:54:04 +0200

    Voice audio track type.

---
+++ Assets/Scripts/ExpertWaves/Audio/Track/VoiceTrack.cs
@@ -0,0 +1,15 @@
+﻿using ExpertWaves.Audio.Enum;
+using UnityEngine;
+
+namespace ExpertWaves {
+	namespace Audio {
+		namespace Track {
+			[System.Serializable]
+			public class VoiceTrack : NoneTrack {
+				public new readonly ITrackType track = ITrackType.Voice;
+				public AudioSource source;
+				public VoiceClip[] clips;
+			}
+		}
+	}
+}
