9778240695b27763f03b389035940a6f5056c806
qaman88 <qaman88@hotmail.com>
2021-08-16 22:50:57 +0200

    Android library to share app through intent.
    Features:
    * Share intent
    
    Fixes:
    * Bug fixing and code optimazation
    * Code comment
    * Code brush up
    * Code clean up
    
    Drops:
    *

---
+++ AndroidStudio/UnityK53JavaPlugin/app/src/main/java/za/co/expertwaves/k53eyetest/unityk53javaplugin/MainActivity.java
@@ -0,0 +1,13 @@
+package za.co.expertwaves.k53eyetest.unityk53javaplugin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
