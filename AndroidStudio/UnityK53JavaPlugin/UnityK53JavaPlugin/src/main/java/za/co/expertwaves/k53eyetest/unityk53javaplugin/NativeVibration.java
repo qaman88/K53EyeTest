9ce6873ef5aa3645d64b3b7ca5dcff600c167277
qaman88 <qaman88@hotmail.com>
2021-08-25 00:51:44 +0200

    Android plugin upgrade to support wave vibration.
    Fixes:
    * Bug fixing and code optimazation
    * Code comment
    * Code brush up
    * Code clean up
    
    Drops:
    *

--- AndroidStudio/UnityK53JavaPlugin/UnityK53JavaPlugin/src/main/java/za/co/expertwaves/k53eyetest/unityk53javaplugin/NativeVibration.java
+++ AndroidStudio/UnityK53JavaPlugin/UnityK53JavaPlugin/src/main/java/za/co/expertwaves/k53eyetest/unityk53javaplugin/NativeVibration.java
@@ -1,12 +1,36 @@
 package za.co.expertwaves.k53eyetest.unityk53javaplugin;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 
 public class NativeVibration {
   private static final String TAG = "[NativeVibration]";
+  private static Activity unityActivity;
+  private static Vibrator vibrator;
 
-  // TODO - implementation of static vibrate method
-  public static void Vibrator(float duration) {
-    // Vibrator.vibrate(VibrationEffect.EFFECT_DOUBLE_CLICK);
+  public static void SetUnityActivity(Activity activity) {
+    unityActivity = activity;
+    vibrator = (Vibrator) unityActivity.getSystemService(Context.VIBRATOR_SERVICE);
   }
- }
+
+  public static void Vibrate(long ms_duration) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      vibrator.vibrate(VibrationEffect.createOneShot(ms_duration, VibrationEffect.DEFAULT_AMPLITUDE));
+    } else {
+      vibrator.vibrate(ms_duration);
+    }
+  }
+
+  public static void Vibrate(long[] pattern, int count) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      VibrationEffect effect = VibrationEffect.createWaveform(pattern, count);
+      vibrator.vibrate(effect);
+    }
+    else{
+      vibrator.vibrate(pattern, count);
+    }
+  }
+}
--- AndroidStudio/UnityK53JavaPlugin/app/src/main/java/za/co/expertwaves/k53eyetest/unityk53javaplugin/MainActivity.java
+++ AndroidStudio/UnityK53JavaPlugin/app/src/main/java/za/co/expertwaves/k53eyetest/unityk53javaplugin/MainActivity.java
@@ -1,13 +1,39 @@
 package za.co.expertwaves.k53eyetest.unityk53javaplugin;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+  private static Vibrator vibrator;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView( R.layout.activity_main);
+    vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+  }
+
+  public void OnButtonClick(View view) {
+    Log.i("$$$ MainActivity", "Button Clicked.");
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+     // works/ vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE));
+
+
+      // wave test
+      long[] mVibratePattern = new long[]{0, 40, 80, 60, 80, 100};
+      VibrationEffect effect = VibrationEffect.createWaveform(mVibratePattern, -1);
+      vibrator.vibrate(effect);
+    }
+    else{
+      // works but deprecated // vibrator.vibrate(100);
+    }
+
   }
 }
